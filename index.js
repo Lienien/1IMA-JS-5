c4e5a195a7944def98bc42e22c28e56d7cec2c59
Lienien <116272320+Lienien@users.noreply.github.com>
2023-08-23 08:29:12 +0200

    Make code

---
+++ index.js
@@ -0,0 +1,37 @@
+//oppgave 1
+// function oppg1(a,b,c) {
+//     return a+b+c;
+// }
+
+// //oppgave 2
+// function oppg2(tall1,tall2,tall3) {
+//     return tall1+tall2+tall3;
+// }
+
+
+// //oppgave 3
+// function oppg3() {
+//     const tall1 = Math.round(Math.random() * 10);
+//     const tall2 = Math.round(Math.random() * 10);
+
+//     if (tall1 > tall2) {
+//         return(tall1 + " er større enn" + tall2)
+//     } else if (tall1 < tall2) {
+//         return(tall2 + "er større enn " + tall1)        
+//     } else {
+//         return(tall1 + "er like stor som " + tall2)
+//     }
+// }
+
+// console.log(oppg3())
+
+//oppgave 4
+function oppg4() {
+    let tall1 = prompt("Skriv det første tallet")
+    let tall2 = prompt("Skriv det andre tallet")
+    let tegn = prompt("Skriv +, -, * eller /")
+
+    sum = tegn
+}
+
+console.log(oppg4())
